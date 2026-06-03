7f9202dda5b9b0c907f8020f975554db026f1e1a
Zafuzi <zacharyfoutz@gmail.com>
2026-06-03 09:25:21 -0700

    lots of clean, minimal v1

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import express from "express";
+import path from "node:path";
+import { engine } from "express-handlebars";
+import bodyParser from "body-parser";
+import { Resend } from "resend";
+import "dotenv/config";
+import { Sanitizer } from "sanitize";
+import strip from "strip";
+
+const app = express();
+const jsonParser = bodyParser.json();
+const resend = new Resend(process.env.RESEND_API_KEY);
+const sanitizer = new Sanitizer();
+
+app.use("/public", express.static(path.join(import.meta.dirname, 'public')));
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(import.meta.dirname, "views"));
+
+app.get("/", (req, res) => {
+	res.render("home");
+})
+
+app.post("/send_message", jsonParser, (req, res) => {
+	const message = strip(sanitizer.str(req.body.message));
+	const name = strip(sanitizer.str(req.body.name));
+	const email = strip(sanitizer.str(req.body.email));
+
+	resend.emails.send({
+		from: "contact@mail.zafuzi.dev",
+		to: "zacharyfoutz@gmail.com",
+		template: {
+			id: "website-contact",
+			variables: {
+				sender_name: name,
+				sender_email: email,
+				message
+			}
+		},
+	});
+
+	res.status(200);
+	res.end();
+})
+
+app.listen(3001, function () {
+	console.log('Your app is listening on port 3001');
+});
