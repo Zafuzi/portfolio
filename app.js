4dfadbc605162d260c7c436204f530eaf8f9d60e
Zafuzi <zacharyfoutz@gmail.com>
2026-07-30 09:47:30 -0700

    updated port

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ app.post("/send_message", jsonParser, (req, res) => {
 	okay("message sent", req, res);
 });
 
-app.listen(3001, function () {
+app.listen(3000, function () {
 	console.log('Your app is listening on port 3001');
 });
