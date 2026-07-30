0e430fed0c01d38636691c885583a1e09b1ea276
Zafuzi <zacharyfoutz@gmail.com>
2026-07-30 10:21:28 -0700

    Moved to php static site

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Zachary Harper - Software Engineer</title>
+</head>
+<body>
+	<header>
+		<h1>Zachary Harper <sub>Software Engineer</sub></h1>	
+	</header>
+	<main>
+		<article>
+			<section>
+				<h2>Links</h2>
+				<ul>
+					<li>
+						<a href="https://github.com/zafuzi">Github</a>
+					</li>
+				</ul>
+			</section>
+		</article>
+	</main>
+</body>
+</html>
